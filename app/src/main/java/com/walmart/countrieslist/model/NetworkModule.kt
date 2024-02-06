e65e447d9404ac755e3019f06a6da23981d04541
Dhanush <dhanushpakanati@gmail.com>
2024-02-05 18:02:48 -0600

    added connection timeouts

--- app/src/main/java/com/walmart/countrieslist/model/NetworkModule.kt
+++ app/src/main/java/com/walmart/countrieslist/model/NetworkModule.kt
@@ -1,12 +1,20 @@
 package com.walmart.countrieslist.model
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object NetworkModule {
     private fun createRetrofit(): Retrofit {
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+        //could further add a retry interceptor
         return Retrofit.Builder()
             .baseUrl("https://gist.githubusercontent.com/")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
